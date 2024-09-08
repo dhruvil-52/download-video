27c53c7cd5a9ad54c7ad4d4d4152c6c21e1295c0
Dhruvil <Dhruvil@buildesk.in>
2024-09-08 18:33:57 +0530

    changes

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const { alldl } = require("rahad-all-downloader");
+const { alldown } = require("nayan-media-downloader");
+const { ytdown } = require("nayan-media-downloader");
+const { ndown } = require("nayan-media-downloader");
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Add this to parse JSON request bodies
-
 app.listen(PORT, () => {
   console.log(`Server Works !!! At port ${PORT}`);
 });
@@ -15,20 +16,22 @@ app.listen(PORT, () => {
 app.post("/downloadmp4", async (req, res) => {
   const url = req.body.data.url;
   console.log(url);
-  alldl(url).then(
-    (resp) => {
-      console.log("22",resp.data);
-      if (resp.data && resp.data.videoUrl) {
-        let response = resp.data.videoUrl;
-        res.status(200).send({ downloadVideoUrl: response });
-      } else {
-        console.log("25");
-        res.status(500).send("Error downloading video");
-      }
-    },
-    (err) => {
-      console.log("err 29", err);
-      res.status(500).send("Error downloading video");
+
+  try {
+    console.log("here");
+    let resp = await alldown(url);
+    console.log(resp);
+    res.status(200).send(resp.data);
+  } catch (error) {
+    try {
+      let resp = await ytdown(url);
+      console.log(resp);
+      res.status(200).send(resp.data);
+    } catch {
+      let resp = await ndown(url);
+      console.log(resp);
+      res.status(200).send(resp.data);
     }
-  );
+    res.status(500).send("Error downloading video");
+  }
 });
